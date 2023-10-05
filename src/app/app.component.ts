af0bf844ac7626aca39f2234611a1599736c747c
wl.ong <wl.ong@outlook.com>
2023-10-05 14:27:11 +0800

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { faker } from '@faker-js/faker'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence()
+  enteredText = ''
+  solved: boolean = false
+
+  onInput(value: string) {
+    this.enteredText = value
+
+    if (value === this.randomText) {
+      this.solved = true
+    } else {
+      this.solved = false
+    }
+  }
+
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) {
+      return 'pending'
+    }
+
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect'
+  }
 }
